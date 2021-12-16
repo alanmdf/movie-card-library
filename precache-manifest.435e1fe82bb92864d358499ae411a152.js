9196a699f120a6cd805e026171621d45748760a4
alanmdf <alanmoreirascr@gmail.com>
2021-12-16 14:15:56 -0300

    Updates

---
+++ precache-manifest.435e1fe82bb92864d358499ae411a152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d277737ccc468f423a0daa4dff33e4de",
+    "url": "/movie-card-library/index.html"
+  },
+  {
+    "revision": "f689be4a74a203864a07",
+    "url": "/movie-card-library/static/css/main.76f102db.chunk.css"
+  },
+  {
+    "revision": "0f87b514ca5c93bba471",
+    "url": "/movie-card-library/static/js/2.47898004.chunk.js"
+  },
+  {
+    "revision": "f689be4a74a203864a07",
+    "url": "/movie-card-library/static/js/main.b9134768.chunk.js"
+  },
+  {
+    "revision": "7344611fcd604ad1faac",
+    "url": "/movie-card-library/static/js/runtime-main.00261771.js"
+  }
+]);
